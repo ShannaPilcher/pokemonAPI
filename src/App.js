5f34ffdfcb57af61121aca3137165ef6c153a25b
Shanna Pilcher <shannampilcher@gmail.com>
2021-03-07 21:46:40 -0700

    added an api request added a button that mapped the results of the api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react'
 
 function App() {
+  const[pokemonList, setPokemonList] = useState([]);
+
+  const getPokemon = () => {
+    fetch("https://pokeapi.co/api/v2/pokemon/?limit=807")
+      .then((response) => response.json())
+      .then((jsonResponse) => {
+        setPokemonList(jsonResponse.results)
+      })
+      .catch((error) => {
+        console.log ("Oops. We couldn't catch any pokemon");
+        console.log(error);
+      })
+  };
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick= {getPokemon}>Click Here for Pokemon</button>
+      {
+        pokemonList.map ((pokemon, index) => (
+        <div key={index}>
+          <hr />
+          <h4> {pokemon.name} </h4>
+        </div>)
+        )
+      }
     </div>
   );
 }
