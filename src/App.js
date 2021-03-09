59cf9d1d598f9f45dda558f246666a39449f7484
Shanna Pilcher <shannampilcher@gmail.com>
2021-03-08 18:37:34 -0700

    Changed code to use axios instead of fetch.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
 import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
 function App() {
   const[pokemonList, setPokemonList] = useState([]);
 
   const getPokemon = () => {
-    fetch("https://pokeapi.co/api/v2/pokemon/?limit=807")
-      .then((response) => response.json())
-      .then((jsonResponse) => {
-        setPokemonList(jsonResponse.results)
-      })
+    axios.get("https://pokeapi.co/api/v2/pokemon/?limit=807")
+    // fetch("https://pokeapi.co/api/v2/pokemon/?limit=807")
+      .then((response) => {setPokemonList(response.data.results)})
+      // .then((jsonResponse) => {
+      //   setPokemonList(jsonResponse.results)
+      // })
       .catch((error) => {
         console.log ("Oops. We couldn't catch any pokemon");
         console.log(error);
@@ -19,7 +21,7 @@ function App() {
   
   return (
     <div className="App">
-      <button onClick= {getPokemon}>Click Here for Pokemon</button>
+      <button onClick= {getPokemon}>Fetch Pokemon</button>
       {
         pokemonList.map ((pokemon, index) => (
         <div key={index}>
